3c8d5e2208411cff4cba4a21b0c18308b41c3a6d
saathvikMD <65287434+saathvikMD@users.noreply.github.com>
2026-09-01 15:34:04 +0530

    Adding solution for Walking Home

--- bronze/walking_home.cpp
+++ bronze/walking_home.cpp
@@ -21,170 +21,37 @@ int main() {
             cin >> grid[i];
         }
 
-        int paths = 0;
+        vector<vector<vector<pair<bool, int>>>> paths(n + 1, vector<vector<pair<bool, int>>>(n + 1));
 
-        for (int starting_dir = 0; starting_dir < 2; starting_dir++) {
-            bool direction = starting_dir;
-            int x = 0;
-            int y = 0;
+        paths[0][0].push_back({true, 0});
+        paths[0][0].push_back({false, 0});
 
-            for (int i = (direction ? x : y); i < n; i++) {
-                if (grid[x][y] == 'H') {
-                    break;
-                } else if (x == n - 1 and y == n - 1) {
-                    paths += 1;
-                    break;
-                } else if (x == n - 1) {
-                    bool passed = true;
-
-                    for (int index = y; index < n; index++) {
-                        if (grid[x][index] == 'H') {
-                            passed = false;
-                            break;
-                        }
-                    }
-
-                    if (passed) {
-                        paths += 1;
-                        break;
-                    }
-                } else if (y == n - 1) {
-                    bool passed = true;
-
-                    for (int index = x; index < n; index++) {
-                        if (grid[index][y] == 'H') {
-                            passed = false;
-                            break;
-                        }
-                    }
-
-                    if (passed) {
-                        paths += 1;
-                        break;
-                    }
-                }
-
-                int temp_x = x;
-                int temp_y = y;
-                int temp_direction = direction;
-
-                direction = not direction;
-
-                if (x > 0 or y > 0) {
-                    for (int j = (direction ? x : y); j < n and k > 1; j++) {
-                        if (grid[x][y] == 'H') {
-                            break;
-                        } else if (x == n - 1 and y == n - 1) {
-                            paths += 1;
-                            break;
-                        } else if (x == n - 1) {
-                            bool passed = true;
-
-                            for (int index = y; index < n; index++) {
-                                if (grid[x][index] == 'H') {
-                                    passed = false;
-                                    break;
-                                }
-                            }
-
-                            if (passed) {
-                                paths += 1;
-                                break;
-                            }
-                        } else if (y == n - 1) {
-                            bool passed = true;
-
-                            for (int index = x; index < n; index++) {
-                                if (grid[index][y] == 'H') {
-                                    passed = false;
-                                    break;
-                                }
-                            }
-
-                            if (passed) {
-                                paths += 1;
-                                break;
-                            }
-                        }
-
-                        int temp_x1 = x;
-                        int temp_y1 = y;
-                        int temp_direction1 = direction;
-
-                        direction = not direction;
-
-                        if (x > temp_x or y > temp_y) {
-                            for (int o = (direction ? x : y); o < n and k > 2; o++) {
-                                if (grid[x][y] == 'H') {
-                                    break;
-                                } else if (x == n - 1 and y == n - 1) {
-                                    paths += 1;
-                                    break;
-                                } else if (x == n - 1) {
-                                    bool passed = true;
-
-                                    for (int index = y; index < n; index++) {
-                                        if (grid[x][index] == 'H') {
-                                            passed = false;
-                                            break;
-                                        }
-                                    }
-
-                                    if (passed) {
-                                        paths += 1;
-                                        break;
-                                    }
-                                } else if (y == n - 1) {
-                                    bool passed = true;
-
-                                    for (int index = x; index < n; index++) {
-                                        if (grid[index][y] == 'H') {
-                                            passed = false;
-                                            break;
-                                        }
-                                    }
-
-                                    if (passed) {
-                                        paths += 1;
-                                        break;
-                                    }
-                                }
-
-                                if (direction) {
-                                    x += 1;
-                                } else {
-                                    y += 1;
-                                }
-
-                            }
-                        }
-
-                        direction = temp_direction1;
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                if (grid[x][y] == '.') {
+                    for (int o = 0; o < paths[x][y].size(); o++) {
+                        bool direction = paths[x][y][o].first;
+                        int turns = paths[x][y][o].second;
 
                         if (direction) {
-                            x = temp_x1 + 1;
-                            y = temp_y1;
+                            paths[x + 1][y].push_back({direction, turns});
+
+                            if (turns < k and x + y > 0) {
+                                paths[x][y + 1].push_back({not direction, turns + 1});
+                            }
                         } else {
-                            y = temp_y1 + 1;
-                            x = temp_x1;
+                            paths[x][y + 1].push_back({direction, turns});
+
+                            if (turns < k and x + y > 0) {
+                                paths[x + 1][y].push_back({not direction, turns + 1});
+                            }
                         }
                     }
                 }
-
-                direction = temp_direction;
-
-                if (direction) {
-                    x = temp_x + 1;
-                    y = temp_y;
-                } else {
-                    y = temp_y + 1;
-                    x = temp_x;
-                }
-
             }
         }
 
-        cout << paths << endl;
+        cout << paths[n - 1][n - 1].size() << "\n";
     }
 
     return 0;
